54972edbe4ed80decf4b5dfd2d7d5abec9de10ba
tatswata <user@appurunoMacBook-Air.local>
2025-02-17 22:17:56 +0900

    不要なメソッド削除

--- src/main/kotlin/com/tatswata/bookmanagement/repository/AuthorRepository.kt
+++ src/main/kotlin/com/tatswata/bookmanagement/repository/AuthorRepository.kt
@@ -47,14 +47,4 @@ class AuthorRepository(private val dsl: DSLContext) {
             return author
         }
     }
-
-    fun update(author: Author): Author {
-        dsl.update(Authors.AUTHORS)
-            .set(Authors.AUTHORS.NAME, author.name.name)
-            .set(Authors.AUTHORS.BIRTH_DATE, author.birthDate.birthDate)
-            .where(Authors.AUTHORS.ID.eq(author.id!!.id))
-            .execute()
-
-        return author
-    }
 }
