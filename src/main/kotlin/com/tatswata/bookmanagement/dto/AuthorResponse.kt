b789bf116b79f4545f386f80820d7898278f6f81
tatswata <user@appurunoMacBook-Air.local>
2025-02-20 21:37:23 +0900

    レスポンス dtoのコンストラクタでドメインオブジェクトを受け取るように変更

--- src/main/kotlin/com/tatswata/bookmanagement/dto/AuthorResponse.kt
+++ src/main/kotlin/com/tatswata/bookmanagement/dto/AuthorResponse.kt
@@ -1,7 +1,15 @@
 package com.tatswata.bookmanagement.dto
 
+import com.tatswata.bookmanagement.domain.author.Author
+
 data class AuthorResponse(
     val id: Int,
     val name: String,
     val birthDate: String
-)
+) {
+    constructor(author: Author) : this(
+        id = author.id!!.id,
+        name = author.name.name,
+        birthDate = author.birthDate.birthDate.toString()
+    )
+}
--- src/main/kotlin/com/tatswata/bookmanagement/dto/BookResponse.kt
+++ src/main/kotlin/com/tatswata/bookmanagement/dto/BookResponse.kt
@@ -1,8 +1,17 @@
 package com.tatswata.bookmanagement.dto
 
+import com.tatswata.bookmanagement.domain.book.Book
+
 data class BookResponse(
     val id: Int,
     val title: String,
     val price: Int,
     val status: String
-)
+) {
+    constructor(book: Book) : this(
+        id = book.id!!.id,
+        title = book.title.title,
+        price = book.price.price,
+        status = book.status.name
+    )
+}
